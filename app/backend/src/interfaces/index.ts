2b535162b42f3cf890865869d81fc973c9dd4625
Richard-Leal <rwsleal@gmail.com>
2022-08-19 16:04:10 -0300

    feat: add some test coverage to the login route

--- app/backend/src/interfaces/index.ts
+++ app/backend/src/interfaces/index.ts
@@ -1,4 +1,5 @@
 import ILogin from './ILogin';
 import IPayload from './IPayload';
+import IUser from './IUser';
 
-export { ILogin, IPayload };
+export { ILogin, IPayload, IUser };
---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,79 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Example from '../database/models/ExampleModel';
+
+import { Response } from 'superagent';
+import Users from '../database/models/Users';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const validLogin = {
+  email: 'admin@admin.com',
+  password: 'secret_admin'
+}
+
+const invalidLogin = {
+  email: '',
+  password: '',
+}
+
+const user = {
+  id: 1,
+  username: 'Admin',
+  role: 'admin',
+  email: 'admin@admin.com',
+  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+}
+
+describe('Login tests', () => {
+  describe('Case the user inputs invalid data', () => {
+
+    describe('if an email is not provided', () => {
+
+      it('should return a bad request status', async () => {
+        const response = await chai.request(app)
+          .post('/login').send(invalidLogin);
+
+        expect(response.status).to.be.equal(400)
+      });
+
+      it('should return an object with a message', async () => {
+        const message = 'All fields must be filled'
+        const response = await chai.request(app)
+          .post('/login').send(invalidLogin);
+
+        expect(response.body).to.have.property('message')
+        expect(response.body.message).to.equal(message)
+      });
+    })
+
+    describe('if an email is not provided', () => {
+
+    })
+  })
+  
+  describe('Case the user inputs valid data', () => {
+
+    beforeEach(() => {
+      sinon.stub(Users, 'findOne').resolves(user as Users);
+    })
+  
+    afterEach(() => {
+      sinon.restore();
+    });
+  
+    it('should return status 200 with a token', async () => {
+      const response = await chai.request(app)
+        .post('/login').send(validLogin);
+      
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.have.property('token');
+    });
+  });
+});
